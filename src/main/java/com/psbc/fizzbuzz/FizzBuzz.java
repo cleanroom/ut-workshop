56bc05b003f0b180bb5be9a983750ada866ed672
mengyuankan <1310208262@qq.com>
2019-12-28 10:30:00 +0800

    red

--- src/main/java/com/psbc/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/psbc/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,38 @@
 package com.psbc.fizzbuzz;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class FizzBuzz {
-    
+
+    /** FizzBuzz */
+    private static final String FIZZ_BUZZ = "FizzBuzz";
+
+    /** Fizz */
+    private static final String FIZZ = "Fizz";
+
+    /** Buzz */
+    private static final String BUZZ = "Buzz";
+
+
+    public String fizzBuzz(int num) {
+        Map<Integer, String> tmpMap = new TreeMap<>();
+
+        for (int i = 1; i <= 100; i++) {
+            if (((i % 3) == 0) && ((i % 5) == 0)) {
+                tmpMap.put(i, FIZZ_BUZZ);
+            } else if ((i % 3) == 0) {
+                tmpMap.put(i, FIZZ);
+            } else if ((i % 5) == 0) {
+                tmpMap.put(i, BUZZ);
+            } else {
+                tmpMap.put(i, String.valueOf(i));
+            }
+        }
+
+        System.out.println(tmpMap.toString());
+        return tmpMap.get(num);
+    }
+
 }
---
+++ src/test/java/com/psbc/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,29 @@
+package com.psbc.fizzbuzz;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * @author 蒙元堪
+ */
+public class FizzBuzzTest {
+
+    /** FizzBuzz 实例 */
+    private FizzBuzz fizzBuzz = null;
+
+    @Before
+    public void init(){
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    public void testError(){
+       String result =  fizzBuzz.fizzBuzz(3);
+        assertEquals("应该返回Fizz",  String.valueOf(3), "Fizz");
+    }
+
+}

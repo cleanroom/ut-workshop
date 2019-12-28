b6e2651a02a9eac58ddbc8e68362da6221e3c190
mengyuankan <1310208262@qq.com>
2019-12-28 13:30:25 +0800

    red

--- src/test/java/com/psbc/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/psbc/fizzbuzz/FizzBuzzTest.java
@@ -1,7 +1,13 @@
 package com.psbc.fizzbuzz;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
@@ -20,23 +26,52 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void testShouldReturnFizz(){
+    public void testShouldReturnFizz() throws IllegalArgumentException {
         assertEquals("Fizz", fizzBuzz.of(3));
     }
 
     @Test
-    public void testShouldReturnBuzz(){
+    public void testShouldReturnBuzz() throws IllegalArgumentException {
         assertEquals("Buzz", fizzBuzz.of(5));
     }
 
     @Test
-    public void testShouldReturnFizzBuzz(){
+    public void testShouldReturnFizzBuzz() throws IllegalArgumentException {
         assertEquals("FizzBuzz", fizzBuzz.of(15));
     }
 
     @Test
-    public void testShouldReturnNumber(){
+    public void testShouldReturnNumber() throws IllegalArgumentException {
         assertEquals("8", fizzBuzz.of(8));
+    }
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void testThrowException() throws IllegalArgumentException {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Invalid input");
+         fizzBuzz.of(0);
+    }
+
+    @Test
+    public void testShouldReturnWhizz(){
+        assertEquals("Whizz", fizzBuzz.of(14));
+    }
+
+    @Test
+    public void testShouldReturnFizzWhizz(){
+        assertEquals("FizzWhizz", fizzBuzz.of(21));
+    }
+
+    @Test
+    public void testShouldReturnBuzzWhizz(){
+        assertEquals("BuzzWhizz", fizzBuzz.of(35));
+    }
+
+    public void testShouldReturnFizzBuzzWhizz(){
+        assertEquals("FizzBuzzWhizz", fizzBuzz.of(105));
     }
 
 }
